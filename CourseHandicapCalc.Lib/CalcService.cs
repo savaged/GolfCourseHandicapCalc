254cc97050d879033d00af1e9a92322d590a35f7
savaged <savagedavid@gmx.com>
2022-08-25 21:31:47 +0100

    fixed

--- CourseHandicapCalc.Lib/CalcService.cs
+++ CourseHandicapCalc.Lib/CalcService.cs
@@ -4,7 +4,7 @@ public static class CalcService
 {
     public static int Calculate(double individualHandicapIndex, double courseRating, int slopeRating, int par)
     {
-        return (int)Math.Round(individualHandicapIndex * ((double)slopeRating / 113 + (courseRating - par)), 0);
+        return (int)Math.Round(individualHandicapIndex * ((double)slopeRating / 113) + (courseRating - par), 0);
     }
     
 }
