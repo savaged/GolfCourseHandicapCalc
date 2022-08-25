05f9dfbd79488f1179e03c03d8e4467f22356fc5
savaged <savagedavid@gmail.com>
2022-08-25 17:13:47 +0100

    setup

---
+++ CourseHandicapCalc.CLI/Program.cs
@@ -0,0 +1,3 @@
+﻿using CourseHandicapCalc.Lib;
+
+Console.WriteLine(CalcService.Calculate(args[0], args[1], args[2], args[3]));
---
+++ CourseHandicapCalc.Lib/CalcService.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseHandicapCalc.Lib;
+
+public static class CalcService
+{
+    public static int Calculate(double individualHandicapIndex, double courseRating, int slopeRating, int par)
+    {
+        return (int)Math.Round(individualHandicapIndex * ((courseRating / 113) + (slopeRating - par)));
+    }
+    
+}
